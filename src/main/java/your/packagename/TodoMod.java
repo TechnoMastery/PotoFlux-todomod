a326e3d95d567bb053ee44ca5e973349845fc184
Minheur2000 <adrien.rousselet@hotmail.com>
2025-12-06 13:00:24 +0100

    started work

--- src/main/java/your/packagename/TodoMod.java
+++ src/main/java/your/packagename/TodoMod.java
@@ -1,10 +1,16 @@
 package your.packagename;
 
+import net.minheur.potoflux.PotoFlux;
 import net.minheur.potoflux.loader.PotoFluxLoadingContext;
 import net.minheur.potoflux.loader.mod.Mod;
 import net.minheur.potoflux.loader.mod.ModEventBus;
 import net.minheur.potoflux.loader.mod.events.RegisterTabsEvent;
 import your.packagename.tabs.Tabs;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Mod(modId = TodoMod.MOD_ID)
 public class TodoMod {
@@ -18,5 +24,13 @@ public class TodoMod {
 
     private void onRegisterTabs(RegisterTabsEvent event) {
         Tabs.register(event.reg);
+    }
+
+    public static Path getModDir() {
+        Path dir = PotoFlux.getProgramDir().resolve("mod-data").resolve("todomod");
+        try {
+            Files.createDirectories(dir);
+        } catch (IOException ignored) {}
+        return dir;
     }
 }
--- src/main/java/your/packagename/tabs/all/TodoTab.java
+++ src/main/java/your/packagename/tabs/all/TodoTab.java
@@ -1,15 +1,134 @@
 package your.packagename.tabs.all;
 
+import net.minheur.potoflux.PotoFlux;
 import net.minheur.potoflux.screen.tabs.BaseTab;
+import net.minheur.potoflux.translations.Translations;
+import net.minheur.potoflux.utils.InputWithCheckboxResult;
+import your.packagename.TodoMod;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static net.minheur.potoflux.Functions.removeProhibitedChar;
 
 public class TodoTab extends BaseTab {
+    private final List<TodoItem> todos = new ArrayList<>();
+    private final JPanel listPanel = new JPanel();
+    private final Path saveFile = TodoMod.getModDir().resolve("todo.json");
+
+    public TodoTab() {
+        super();
+
+        loadTodos();
+        refreshList();
+    }
+
+    @Override
+    protected boolean invokeLater() {
+        return true;
+    }
+
     @Override
     protected void setPanel() {
-        // add here content
+        PANEL.setLayout(new BorderLayout());
+
+        // --- main : title & buttons ---
+        JPanel top = new JPanel();
+        top.setLayout(new BoxLayout(top, BoxLayout.Y_AXIS));
+        top.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        // -- title --
+        JLabel title = new JLabel(getTitle());
+        title.setFont(new Font("Consolas", Font.BOLD, 20));
+        title.setAlignmentX(Component.LEFT_ALIGNMENT);
+        top.add(title);
+        top.add(Box.createVerticalStrut(10));
+
+        // -- add button --
+        JButton addButton = new JButton(Translations.get("tabs.todo.button_add"));
+        addButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+        addButton.addActionListener(e -> addNewTodo());
+        top.add(addButton);
+        top.add(Box.createVerticalStrut(10));
+
+        // -- remove all button --
+        JButton clearButton = new JButton(Translations.get("tabs.todo.button_clear"));
+        clearButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+        clearButton.addActionListener(e -> clearAll());
+        top.add(clearButton);
+
+        PANEL.add(top, BorderLayout.NORTH);
+
+        // --- center : list ---
+        listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.Y_AXIS));
+        listPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        // --- task list ---
+        JScrollPane scroll = new JScrollPane(listPanel);
+        scroll.setBorder(null);
+
+        PANEL.add(scroll, BorderLayout.CENTER);
+    }
+
+    @Override
+    protected boolean doPreset() {
+        return false;
     }
 
     @Override
     protected String getTitle() {
         return "TODO";
+    }
+
+    private void clearAll() {
+        todos.clear();
+        saveTodos();
+        refreshList();
+    }
+
+    private void addNewTodo() {
+        InputWithCheckboxResult input = InputWithCheckboxResult.showInputWithCheckboxDialog(PANEL, Translations.get("tabs.todo.new.title"), Translations.get("tabs.todo.new.name"), Translations.get("tabs.todo.new.pinned")); // TODO
+        String text = input.text;
+        boolean pinned = input.pinned;
+
+        if (text == null || text.trim().isEmpty()) {
+            showTodoError();
+            return;
+        }
+        text = removeProhibitedChar(text);
+
+        if (!text.trim().isEmpty()) {
+            todos.add(0, new net.minheur.potoflux.screen.tabs.all.TodoTab.TodoItem(text.trim(), false, pinned));
+            saveTodos();
+            refreshList();
+        } else showTodoError();
+    }
+
+    private void showTodoError() {
+        JOptionPane.showMessageDialog(PANEL, Translations.get("tabs.todo.no_empty"));
+    }
+
+    private void refreshList() {
+        listPanel.removeAll();
+
+        if (todos.isEmpty()) {
+
+        }
+    }
+
+    private static class TodoItem {
+        String text;
+        boolean done;
+        boolean pinned;
+
+        public TodoItem(String text, boolean done, boolean pinned) {
+            this.text = text;
+            this.done = done;
+            this.pinned = pinned;
+        }
     }
 }
