9d9d1f6164cde08f57175348b0773f3bcc2c8f00
Minheur2000 <adrien.rousselet@hotmail.com>
2025-12-08 17:20:49 +0100

    worked

--- src/main/java/your/packagename/TodoMod.java
+++ src/main/java/your/packagename/TodoMod.java
@@ -4,13 +4,13 @@ import net.minheur.potoflux.PotoFlux;
 import net.minheur.potoflux.loader.PotoFluxLoadingContext;
 import net.minheur.potoflux.loader.mod.Mod;
 import net.minheur.potoflux.loader.mod.ModEventBus;
+import net.minheur.potoflux.loader.mod.events.RegisterLangEvent;
 import net.minheur.potoflux.loader.mod.events.RegisterTabsEvent;
 import your.packagename.tabs.Tabs;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Mod(modId = TodoMod.MOD_ID)
 public class TodoMod {
@@ -20,10 +20,15 @@ public class TodoMod {
         ModEventBus modEventBus = PotoFluxLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(RegisterTabsEvent.class, this::onRegisterTabs);
+        // modEventBus.addListener(RegisterLangEvent.class, this::onRegisterLang);
     }
 
     private void onRegisterTabs(RegisterTabsEvent event) {
         Tabs.register(event.reg);
+    }
+
+    private void onRegisterLang(RegisterLangEvent event) {
+
     }
 
     public static Path getModDir() {
