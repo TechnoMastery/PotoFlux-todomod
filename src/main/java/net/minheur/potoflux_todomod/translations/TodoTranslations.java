55449acd00f1169336ddb0e7fa58fce8a802b1b1
Minheur2000 <adrien.rousselet@hotmail.com>
2025-12-20 17:40:26 +0100

    new impl potoflux, to use

---
+++ src/main/java/net/minheur/potoflux_todomod/translations/TodoTranslations.java
@@ -0,0 +1,15 @@
+package net.minheur.potoflux_todomod.translations;
+
+import net.minheur.potoflux.translations.AbstractTranslationsRegistry;
+import net.minheur.potoflux_todomod.TodoMod;
+
+public class TodoTranslations extends AbstractTranslationsRegistry {
+    public TodoTranslations() {
+        super(TodoMod.MOD_ID);
+    }
+
+    @Override
+    protected void makeTranslation() {
+
+    }
+}
