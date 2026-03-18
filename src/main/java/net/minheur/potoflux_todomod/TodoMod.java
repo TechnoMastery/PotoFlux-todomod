fe323a955faad3e8f5ea0907161c6466b24f41cb
Minheur4000 <adrien.rousselet@hotmail.com>
2026-03-18 10:37:01 +0100

    added missing TodoMod.getVersion

--- src/main/java/net/minheur/potoflux_todomod/TodoMod.java
+++ src/main/java/net/minheur/potoflux_todomod/TodoMod.java
@@ -6,12 +6,15 @@ import net.minheur.potoflux.loader.mod.Mod;
 import net.minheur.potoflux.loader.mod.ModEventBus;
 import net.minheur.potoflux.loader.mod.events.RegisterLangEvent;
 import net.minheur.potoflux.loader.mod.events.RegisterTabsEvent;
+import net.minheur.potoflux.logger.LogCategories;
+import net.minheur.potoflux.logger.PtfLogger;
 import net.minheur.potoflux_todomod.tabs.Tabs;
 import net.minheur.potoflux_todomod.translations.TodoTranslations;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Properties;
 
 @Mod(modId = TodoMod.MOD_ID, version = "1.3", compatibleVersionUrl = "https://technomastery.github.io/PotoFluxAppData/modVersions/todomod.json")
 public class TodoMod {
@@ -34,5 +37,18 @@ public class TodoMod {
             Files.createDirectories(dir);
         } catch (IOException ignored) {}
         return dir;
+    }
+
+    public static String getVersion() {
+        try {
+            Properties props = new Properties();
+            props.load(TodoMod.class.getResourceAsStream("/modVersion.properties"));
+
+            return props.getProperty("version");
+        } catch (IOException e) {
+            e.printStackTrace();
+            PtfLogger.error("Could not get version for mod " + MOD_ID, LogCategories.MOD_LOADER);
+            return null;
+        }
     }
 }
