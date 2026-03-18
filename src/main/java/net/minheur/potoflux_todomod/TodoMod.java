48f02cf708c1aa22682681fd429e5e6f6a14513d
Minheur4000 <adrien.rousselet@hotmail.com>
2026-03-18 10:47:37 +0100

    removed unused import

--- src/main/java/net/minheur/potoflux_todomod/TodoMod.java
+++ src/main/java/net/minheur/potoflux_todomod/TodoMod.java
@@ -5,7 +5,6 @@ import net.minheur.potoflux.loader.PotoFluxLoadingContext;
 import net.minheur.potoflux.loader.mod.Mod;
 import net.minheur.potoflux.loader.mod.ModEventBus;
 import net.minheur.potoflux.loader.mod.events.RegisterLangEvent;
-import net.minheur.potoflux.loader.mod.events.RegisterTabsEvent;
 import net.minheur.potoflux.logger.LogCategories;
 import net.minheur.potoflux.logger.PtfLogger;
 import net.minheur.potoflux_todomod.tabs.Tabs;
